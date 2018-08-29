f7832381d42fee6ba20e058e77a3a3a750e11add
ysqyang <ysqyang@gmail.com>
2018-08-29 18:44:59 -0400

    incorrect logic for computing detection ratio

--- detection_ratio.py
+++ detection_ratio.py
@@ -7,15 +7,19 @@ lines = open('1sec_tx.txt', 'r')
 for line in lines:
     line = line.strip().split(' ')
     d1.append(line[7])
-lines = open('10ms_tx.txt', 'r')
+lines = open('100ms_tx.txt', 'r')
 for line in lines:
     line = line.strip().split(' ')
     d2.append(line[7])
 
 count = 0
 prev = 0
-for i in d1:
-    if float(i) > -80:
+th = -70
+low = 1
+for i in d2:
+    if float(i) > th and low == 1:
         count += 1
-        prev = i
+        low = 0
+    if float(i) < th:
+       low = 1
 print count
