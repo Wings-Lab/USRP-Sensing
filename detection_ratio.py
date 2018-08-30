8d4b7dc6c89f0457ff5eb2ac615f86025917a1ba
mdasari823 <mdasari@cs.stonybrook.edu>
2018-08-29 20:25:01 -0400

    adding detection ratio of two experiments usrp fft

--- detection_ratio.py
+++ detection_ratio.py
@@ -1,25 +1,29 @@
+import os
 import numpy as np
 
-d1 = []
-d2 = []
+data = []
 
-lines = open('1sec_tx.txt', 'r')
-for line in lines:
-    line = line.strip().split(' ')
-    d1.append(line[7])
-lines = open('100ms_tx.txt', 'r')
-for line in lines:
-    line = line.strip().split(' ')
-    d2.append(line[7])
+files = ['100ms_run1.txt', '100ms_run3.txt', '100ms_run4.txt', '100ms_run5.txt', '100ms_run2.txt']
+#files = ['10ms_run1.txt', '10ms_run3.txt', '10ms_run4.txt', '10ms_run2.txt', '10ms_run5.txt']
 
-count = 0
-prev = 0
+for f in files: 
+    lines = open('usrp_fft/'+f, 'r')
+    d1 = []
+    for line in lines:
+        line = line.strip().split(' ')
+        d1.append(line[7])
+    data.append(d1)
+
 th = -70
-low = 1
-for i in d2:
-    if float(i) > th and low == 1:
-        count += 1
-        low = 0
-    if float(i) < th:
-       low = 1
-print count
+counts = []
+for d in data:
+    count = 0
+    low = 1
+    for i in d:
+        if float(i) > th and low == 1:
+            count += 1
+            low = 0
+        if float(i) < th:
+           low = 1
+    counts.append(count)
+print counts, np.mean(counts)
