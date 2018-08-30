343b23ecc024f63a3454e059b131e4a6c1444ee2
mdasari823 <mdasari@cs.stonybrook.edu>
2018-08-29 23:16:19 -0400

    adding plot for usrp fft: IQ is pending

--- detection_ratio.py
+++ detection_ratio.py
@@ -34,24 +34,33 @@ def countTx(data):
 
 files1 = ['100ms_run1.txt', '100ms_run3.txt', '100ms_run4.txt', '100ms_run5.txt', '100ms_run2.txt']
 files2 = ['10ms_run1.txt', '10ms_run3.txt', '10ms_run4.txt', '10ms_run2.txt', '10ms_run5.txt']
+files3 = ['1ms_run1.txt', '1ms_run3.txt', '1ms_run4.txt', '1ms_run2.txt', '1ms_run5.txt']
+files4 = ['1us_run1.txt', '1us_run3.txt', '1us_run4.txt', '1us_run2.txt', '1us_run5.txt']
 
 data = getData(files1)
 counts1 = countTx(data)
 data = getData(files2)
 counts2 = countTx(data)
+data = getData(files3)
+counts3 = countTx(data)
+data = getData(files4)
+counts4 = countTx(data)
 
 print counts1, np.mean(counts1)
 print counts2, np.mean(counts2)
+print counts3, np.mean(counts3)
+print counts4, np.mean(counts4)
 
-tx = [np.mean(counts1), np.mean(counts2)]
+tx = [100, np.mean(counts1), np.mean(counts2), np.mean(counts3), np.mean(counts4)]
+tx = [i for i in reversed(tx)]
 idx = np.arange(len(tx))
-print idx
 width = 0.3
 
 plt.bar(idx, tx, width)
 
 plt.ylim([0, 100])
-plt.xlabel('Transmission Time (ms)')
+plt.xlabel('Transmission Length (ms)')
 plt.ylabel('Detection Ratio')
+plt.xticks(np.arange(len(tx)), [0.001, 1, 10, 100, 1000])
 
 plt.show()
