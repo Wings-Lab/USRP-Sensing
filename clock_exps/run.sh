28dddf408d0177ab0dbc9bc966f7263489f64246
mdasari823 <mdasari@cs.stonybrook.edu>
2018-09-04 00:04:10 -0400

    adding clock exps

--- clock_exps/run.sh
+++ clock_exps/run.sh
@@ -1,3 +1,3 @@
-cpufreq-set -f 1296000
+cpufreq-set -f 10000000
 sleep 1
-/usr/share/gnuradio/examples/uhd/usrp_spectrum_sense.py 915.3M 916.3M -s 1M > 1296_run1.txt
+/usr/share/gnuradio/examples/uhd/usrp_spectrum_sense.py 915.3M 916.3M -s 1M > 100_run6.txt
