5fce47544d93bd7de1ba104d0105665f65439211
mdasari823 <mdasari@cs.stonybrook.edu>
2018-10-13 23:31:59 -0400

    edited background

--- fusion/static/plot.py
+++ fusion/static/plot.py
@@ -3,7 +3,7 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 
-matplotlib.rcParams.update({'font.size':40})
+matplotlib.rcParams.update({'font.size':44})
 matplotlib.rcParams['figure.figsize'] = 20, 10
 
 fig = plt.figure()
@@ -12,8 +12,8 @@ fig.subplots_adjust(left=0.12, bottom=0.2, right=0.95)
 
 s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-ms = [30, 39, 58, 61, 63, 86, 88, 95, 98, 98]
-us = [18, 32, 38, 54, 58, 76, 82, 87, 91, 96]
+ms = [45, 49, 58, 61, 72, 86, 89, 97, 98, 98]
+us = [34, 35, 38, 54, 58, 76, 82, 87, 91, 96]
 
 plt.plot(s, ms, linewidth=10, marker='s', markersize=28, markeredgecolor='black', label='1ms')
 plt.plot(s, us, linewidth=10, marker='o', markersize=28, markeredgecolor='black', label='1us')
